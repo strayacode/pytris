5375281613667d137c98fafab8aa026bdff98be1
strayacode <33187392+strayacode@users.noreply.github.com>
2019-12-21 16:36:41 +1100

    Add files via upload

--- tetris.py
+++ tetris.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import time
 
 win_width = 1280
 win_height = 720
@@ -8,13 +9,13 @@ board_start_y = 10
 block_size = 30
 board_width = 10 * block_size
 board_height = 20 * block_size
-
+temp_positions = []
 locked_positions = {}
-
+grid = {}
 S = [['.....',
-      '......',
-      '..00..',
-      '.00...',
+      '.....',
+      '..00.',
+      '.00..',
       '.....'],
      ['.....',
       '..0..',
@@ -114,17 +115,20 @@ T = [['.....',
       '.....']]
 
 shapes = [S, Z, I, O, J, L, T]
-shape_colors = [(0, 255, 0), (255, 0, 0), (0, 255, 255), (255, 255, 0), (255, 165, 0), (0, 0, 255), (128, 0, 128)]
+shape_colours = [(178, 250, 180), (222, 98, 91), (74, 178, 199), (227, 221, 107), (235, 167, 84), (104, 174, 217), (208, 111, 227)]
 
 def create_grid():
-	grid = {}
+	global grid
 	for i in range(10):
 		for j in range(20):
 			grid[(i, j)] = (0, 0, 0)
 	return grid 
 
 def draw_grid(window):
-	grid = create_grid()
+	global locked_positions
+	global grid
+	for block in locked_positions:
+		grid[block] = locked_positions[block]
 	for block in grid:
 		pygame.draw.rect(window, grid[block], [board_start_x + block[0] * block_size, board_start_y + block[1] * block_size, block_size, block_size], 0)
 
@@ -136,39 +140,127 @@ def draw_window(window):
 			pygame.draw.line(window, (82,82,82), (board_start_x,board_start_y + j*block_size), (board_start_x + 10*block_size, board_start_y + j*block_size), 2)
 	
 def convert_block(x, y, shape, rotation):
+	global grid, temp_positions
 	positions = []
-	case = shape[rotation]
+	if len(temp_positions) > 0:
+		for position in temp_positions:
+			grid[position] = (0, 0, 0)
+	case = shape[rotation % len(shape)]
 	for i in range(len(case)):
-		for j in case[i]:
-			if j == "0":
+		for j in range(len(case)):
+			if case[i][j] == "0":
+				positions.append((i + x - 2, j + y - 1))
+	temp_positions = positions
+	for position in positions:
+		grid[position] = shape_colours[shapes.index(shape)]
 
-def create_shape(x, y):
+	return positions
+
+def create_shape(x, y, rotation):
 	shape = get_shape()
-	return x, y, shape
+	return x, y, rotation, shape
 
 
 def get_shape():
 	return random.choice(shapes)
 
+def check_lost():
+	global locked_positions
+	for position in locked_positions:
+		if position[1] <= 0:
+			return True
+	return False
 
 def clear_row():
-	pass
+	global grid
+	"""
+	plan:
+	DONE check if any rows have all values which are not 0, 0, 0
+	delete that row
+	insert a row before everything
+	"""
+	colours = []
+	for position in grid:
+		colours.append(grid[position])
+	for i in range(20):
+		print(colours[i*10:(i*10)+10])
+				
+		# for j in range(20):
+		# if (0, 0, 0) not in colours[i*10:(i*10) + 10]:
 
+			# del colours[i*10:(i*10) + 10]
+			# for j in range(10):
+				# colours.append((0, 0, 0))
+			
+		# print(colours)
+	# print(colours)
+def collide_detect(x, y, shape, rotation):
+	global grid, locked_positions
+	valid_positions = []
+	for position in grid:
+		if position not in locked_positions:
+			valid_positions.append(position)
 
+	positions = convert_block(x, y, shape, rotation)
+	for position in positions:
+		if position not in valid_positions:
+			if position[1] > 0:
+				return True
+		
+	return False
+		
+	
 def main(window): 
 	run = True
 	clock = pygame.time.Clock()
-	x, y, shape = create_shape(5, 0)
+	x, y, rotation, shape = create_shape(5, 0, 0)
 	rotation = 0
-	
+	grid = create_grid()
+	fall_time = 0
 	while run:
+		fall_time += clock.get_time()
+		if fall_time % 250 == 0:
+			y += 1
+			if collide_detect(x, y, shape, rotation):
+				y -= 1
+				for position in positions:
+					locked_positions[position] = shape_colours[shapes.index(shape)]
+				x, y, rotation, shape = create_shape(5, 0, 0)
+				fall_time = 0
+				clear_row()
+		positions = convert_block(x, y, shape, rotation)
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				run = False
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_LEFT:
+					x -= 1
+					if collide_detect(x, y, shape, rotation):
+						x += 1
+				if event.key == pygame.K_RIGHT:
+					x += 1
+					if collide_detect(x, y, shape, rotation):
+						x -= 1
+				if event.key == pygame.K_DOWN:
+					y += 1
+					if collide_detect(x, y, shape, rotation):
+						y -= 1
+						for position in positions:
+							locked_positions[position] = shape_colours[shapes.index(shape)]
+						x, y, rotation, shape = create_shape(5, 0, 0)
+						fall_time = 0
+						clear_row()
+				if event.key == pygame.K_f:
+					rotation -= 1
+				if event.key == pygame.K_d:
+					rotation += 1
+		positions = convert_block(x, y, shape, rotation)
 		draw_window(window)
+		if check_lost():
+			run = False
 		pygame.display.update()
-		convert_block(x, y, shape, rotation)
-		clock.tick(30)
+		
+		clock.tick()
 
 
 def main_menu():
